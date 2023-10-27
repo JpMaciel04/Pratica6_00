625e4015fdd50ea6fc4c60215361599d521c699f
João Pedro Maciel de Oliveira <12115835@aluno.newtonpaiva.br>
2023-10-26 21:47:12 -0300

    Initial commit

---
+++ src/br/com/Questao02/Pessoa.java
@@ -0,0 +1,13 @@
+package br.com.Questao02;
+
+public class Pessoa {
+    public String nome;
+    public String email;
+    public String telefone;
+
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
